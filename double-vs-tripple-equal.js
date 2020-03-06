d4e6131440b500c405394903502a0c6f893ecd08
merahra <merahra@hotmail.com>
2020-03-06 19:10:59 +0600

    something added

---
+++ double-vs-tripple-equal.js
@@ -0,0 +1,17 @@
+const first = 2;
+const second = "2"
+if(first == second){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
+
+const firstt = 2;
+const secondd = "2"
+if(firstt === secondd){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
---
+++ null-vs-undefined.js
@@ -0,0 +1,16 @@
+let pakhi
+console.log(pakhi);
+
+function add(num1,num2){
+    console.log(num1+num2);
+}
+const result = add(12,22);
+console.log(result);
+
+/**
+ * undefine ekta debug error term 
+ * jeta ase variable st na korle , property na thakle, array er maje akta kisu nai
+ * kintu seta dekhte chassi tahole undefined dekhabe
+ * 
+ * null ekta keywords jeta developer ra set kore dai
+ */
